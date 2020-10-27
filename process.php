4fc9463d4e73d7aa819f822bce0bb3c0bdcdac92
CodeItNow123 <73362741+CodeItNow123@users.noreply.github.com>
2020-10-27 11:58:02 -0700

    Update process.php

--- process.php
+++ process.php
@@ -1,4 +1,5 @@
 <?php
-mail($_POST["email"],"Email Confirmation","Hello, " + $_POST["First Name"] + ", you have signed up for Great Talk. Please click <a href='https://great-talk.github.io'>this link</a> to confirm.","From: signup@grea-talk.github.io");
+mail($_POST["email"],"Email Confirmation","Hello, " + $_POST["First Name"] + ", you have signed up for Great Talk. Please click <a href='https://great-talk.github.io'>this link</a> to confirm.","From: signup@great-talk.github.io");
+echo ("A message has been sent to your inbox. If you did not recieve it, click ")
 ?>
-A message has been sent to your inbox. If you did not recieve it, click <button onClick="history.reload()">this button</button>.
+<button onClick="history.reload()">this button</button>.
