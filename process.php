70060474e4fd7d37d244e49bfc8b4f6d482eb746
CodeItNow123 <73362741+CodeItNow123@users.noreply.github.com>
2020-10-27 11:51:05 -0700

    Update process.php

--- process.php
+++ process.php
@@ -1,3 +1,4 @@
 <?php
-mail(to,subject,message,headers);
+mail($_POST["email"],"Email Confirmation","Hello, " + $_POST["First Name"] + ", you have signed up for Great Talk. Please click <a href='https://great-talk.github.io'>this link</a> to confirm.","From: signup@grea-talk.github.io");
 ?>
+A message has been sent to your inbox. If you did not recieve it, click <button onClick="history.reload()">this button</button>.
