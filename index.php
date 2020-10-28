bdd934f99e14be07b91b79072c6714265167de08
CodeItNow123 <73362741+CodeItNow123@users.noreply.github.com>
2020-10-27 17:56:58 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <head>
-<title>Great Talk - Where People Discuss, Chat, and Ask.</title>
+<title>Great Talk &dash; Where People Discuss, Chat, and Ask.</title>
 <meta charset="UTF-8" />
 <meta name="description" content="Using Great Talk, you will be able to: dicuss, chat, and ask. We encourge you reading our guidelines." />
 <link rel="stylesheet" src="css/main-style.css">
 </head>
 <body>
-<h1>Great Talk - Where People Discuss, Chat, and Ask.</h1>
+<h1>Great Talk &dash; Where People Discuss, Chat, and Ask.</h1>
 <p><a href="discuss.html">Discuss</a></p>
 <p><a href="chat.html">Chat</a></p>
 <p><a href="ask.html">Ask</a></p>
